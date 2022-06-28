17e4b6f4ebbeb2b7ce0e047cdb444db3f7f91761
prprnya <nya@ruavan.com>
2022-06-28 12:58:56 +0800

    Add nuxt utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: ['@nuxt/content', '@nuxtjs/color-mode'],
+  content: {
+    markdown: {
+      toc: {
+        depth: 5,
+        searchDepth: 5,
+      },
+    },
+  },
 })
